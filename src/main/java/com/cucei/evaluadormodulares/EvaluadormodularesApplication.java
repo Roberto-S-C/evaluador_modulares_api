7664964e73f0ddb29cca9063ae0544c7794c3091
Roberto-S-C <robertosanchezcazares@gmail.com>
2025-07-08 09:08:31 -0600

    First Commit

---
+++ src/main/java/com/cucei/evaluadormodulares/EvaluadormodularesApplication.java
@@ -0,0 +1,13 @@
+package com.cucei.evaluadormodulares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluadormodularesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluadormodularesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cucei/evaluadormodulares/EvaluadormodularesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cucei.evaluadormodulares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluadormodularesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
